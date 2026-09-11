7aa68e2797f633fc6c847c790745634beb4e43c1
seunex17 <seunexseun@gmail.com>
2026-09-11 10:03:47 +0100

    added play sore images

--- resources/js/components/landing/DownloadSection.svelte
+++ resources/js/components/landing/DownloadSection.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-    import { __ } from '../../lib/i18n';
-    import { reveal } from '../../lib/reveal';
-    import LandingIcon from './LandingIcon.svelte';
+    import { __ } from "../../lib/i18n";
+    import { reveal } from "../../lib/reveal";
+    import LandingIcon from "./LandingIcon.svelte";
 </script>
 
 <section id="download" class="section download-section">
     <div class="page-width download-panel" use:reveal>
         <div class="eyebrow">ONWAYSPAY · PAYMENT CENTER</div>
-        <h2>{__('landing.cta.title')}</h2>
-        <p class="intro">{__('landing.cta.body')}</p>
+        <h2>{__("landing.cta.title")}</h2>
+        <p class="intro">{__("landing.cta.body")}</p>
         <div class="store-buttons">
-            <button class="btn" disabled><LandingIcon name="download" size={18} /> {__('landing.cta.play')}</button>
-            <button class="btn" disabled><LandingIcon name="download" size={18} /> {__('landing.cta.apple')}</button>
+            <img src="/images/svg/google.svg" alt="" class="h-14" />
+            <img src="/images/svg/apple.svg" alt="" class="h-14" />
         </div>
     </div>
 </section>
